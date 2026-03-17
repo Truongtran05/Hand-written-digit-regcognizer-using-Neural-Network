43773fe004b1fd1f44a545c69449649940e9c8e9
Truongman <txtruong2005@gmail.com>
2026-03-17 14:07:50 +0700

    smallchange

--- backend/predictTest.py
+++ backend/predictTest.py
@@ -12,10 +12,10 @@ test_prediction = np.argmax(model.predict(X_test),axis=1)
 precision = np.count_nonzero(test_prediction == y_test)/np.size(y_test)
 print(f"Precision on test data: {precision*100.0}%")
 
-# plt.figure(figsize=(10,5))
-# for i in range(5):
-#     plt.subplot(1,5,i+1)
-#     plt.imshow(X_test[i].reshape(28,28),cmap='gray')
-#     plt.title(f"lable:{test_prediction[i]}")
-#     plt.axis('off') 
-# plt.show()
+plt.figure(figsize=(10,5))
+for i in range(5):
+    plt.subplot(1,5,i+1)
+    plt.imshow(X_test[i].reshape(28,28),cmap='gray')
+    plt.title(f"lable:{test_prediction[i]}")
+    plt.axis('off') 
+plt.show()
